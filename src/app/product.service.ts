9ad58247d98b70fd695d4f65d69dad2c6a420dd3
Jacob Daniels-Flechtner <jacobdanielsflechtner@chevron.com>
2022-03-07 13:23:57 -0600

    Finished step 3

---
+++ src/app/product.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+
+@Injectable()
+export class ProductService {
+  private _albumUrl = "../assets/album.json";
+
+  constructor(private _http: HttpClient) { }
+
+  public getAlbum(id: number)
+  {
+    return this._http.get(this._albumUrl);
+  }
+}
